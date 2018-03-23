41d0295499d236e7f7d0adedabd86bd24a4744a7
fuzzygwalchmei <marc.falzon@gmail.com>
2018-03-22 22:40:27 -0700

    Friday 23rd Update

--- KivyTestMain.py
+++ KivyTestMain.py
@@ -28,25 +28,26 @@ class Kivy_Test_Login(Screen):
         parameters = {"email": doUserName, "password": doPassWord}
         global myUserID
         global AuthToken
+        global myAttributes
 
-
+        #MB API Login
         response = requests.post("http://staging.purchaseplus.com/access/api/auth/sign_in",parameters)
         dataset = response.json()
         print(dataset.keys())
-
         response_status=response.status_code
-        AuthToken = {"access_token": response.headers["access-token"], "uid":response.headers["uid"], "client": response.headers["client"]}
-        response_content = dataset["data"]
-        myAttributes = response_content["attributes"]
 
-
+        #Check for response
         if response.status_code == 200:
-            self.ids.BottomLabel.text = str(response_status)
             self.parent.current = "Main_Screen"
+            response_content = dataset["data"]
+            myAttributes = response_content["attributes"]
             myUserID = response_content["id"]
             current_organisation = myAttributes["default_organisation"]["name"]
-
-
+            print(myAttributes["default_organisation"]["id"]," - ",current_organisation)
+            self.parent.get_screen("Main_Screen").ids.UserButton.text=response.headers["uid"]
+            self.parent.get_screen("Main_Screen").ids.OrgButton.text=current_organisation
+            AuthToken = {"access_token": response.headers["access-token"], "uid":response.headers["uid"], "client": response.headers["client"]}
+        #post status error if failed
         else:
             self.ids.BottomLabel.text = "Your username or password was incorrect. Status: " + str(response_status)
 
@@ -57,29 +58,36 @@ class Kivy_Test_Main(Screen):
     global orgList
     orgList = {}
 
+
+    def getDataList(self, url):
+        baseURL = "http://staging.purchaseplus.com"
+        response = requests.get(baseURL+url, headers=AuthToken)
+        responseJSON = response.json()
+        returnData = responseJSON["data"]
+        return returnData
+
+
+    #Display User information
     def showUserDetails(self):
-        response = requests.get("http://staging.purchaseplus.com/access/api/users/"+myUserID, headers=AuthToken)
-        responseJSON = response.json()
-        userDetails = responseJSON["data"]
-        print(userDetails.keys())
-        for each in userDetails.keys():
-            print(userDetails[each])
-        self.Main_Body.text = userDetails
+        functionURL="/access/api/users/"+myUserID
+        userDetails = self.getDataList(functionURL)
+        self.ids.MainBody.text = "My UID: "+str(myUserID)+"\n"+"My Email: "+myAttributes["email"]
 
 
-
+    #Display a list of Orgs
     def getOrgList(self):
-        response = requests.get("http://staging.purchaseplus.com/access/api/users/"+myUserID+"/organisations", headers=AuthToken)
-        responseJSON = response.json()
-        orgDataset = responseJSON["data"]
-        for each in orgDataset:
-            newkey = each["attributes"]["name"]
-            newdata = each["id"]
+        myOrgList = {}
+        functionURL="/access/api/users/"+myUserID+"/organisations"
+        orgDataset = self.getDataList(functionURL)
+        for orgItem in orgDataset:
+            newkey = orgItem["attributes"]["name"]
+            newdata = orgItem["id"]
             orgList[newkey] = newdata
             self.myOrgList.adapter.data.extend([newkey])
         self.myOrgList._trigger_reset_populate()
 
-    pass
+    def orgClicked(self):
+        self.ids.MainBody.text = "clicked"
 
 
 #Build Main App
