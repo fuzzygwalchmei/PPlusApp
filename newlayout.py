bad557f4969461a907257801c10505e85a067c8a
fuzzygwalchmei <marc.falzon@gmail.com>
2018-03-29 15:01:16 +1100

    Adding new layout

---
+++ newlayout.py
@@ -0,0 +1,14 @@
+from kivy.app import app
+from kivy.uix.
+
+
+
+def NLLogin(Screen)
+
+
+def newlayoutApp(App):
+    def build(self):
+        return NLLogin
+
+if __name__ = "__main__":
+    newlayoutApp().run()
